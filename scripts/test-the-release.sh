1fa1f65ea4b05f125ee531ec43d3479cd62749e4
Lucas Krebs-Pinto <lucaspinto33@gmail.com>
2019-12-03 11:46:40 +0100

    Improve the test script (tbc)

--- scripts/test-the-release.sh
+++ scripts/test-the-release.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
-set -e
+set -o errexit
+set -o pipefail
+IFS=$'\n\t'
 
-export PGHOST="$(bosh vms -d postgres --json| jq -r '.Tables[].Rows[] | select(.instance | contains("postgres")) | .ips')"
+set -x
+
+if [ -z "$2" ]; then
+  echo "Not enough arguments supplied."
+  echo "usage: ./test-the-release.sh deployment-name director-name"
+  exit 1
+fi
+
+set -o nounset
+
+export deployment_name=$1
+export director_name=$2
+
+export PGHOST="$(bosh vms -d ${deployment_name} --json | jq -r '.Tables[].Rows[] | select(.instance | contains("postgres")) | .ips')"
 export PGPORT=5524
-export PGPASSWORD="$(credhub get -n /bosh-lite/postgres/pgadmin_database_password -j | jq ".value" -r)"
+export PGPASSWORD="$(credhub get -n /${director_name}/${deployment_name}/pgadmin_database_password -j | jq ".value" -r)"
 export PGUSER="pgadmin"
-export PGDATABASE="sandbox"
+export PGDATABASE="sandboxes"
+
+if [[ -z $PGPASSWORD ]]; then
+  echo "No password found in CredHub! Is your deployment generating a value for 'pgadmin_dadabase_password'?"
+  echo "Try to deployment with the templates/operations/set-properties.yml ops file."
+  exit 1
+fi
 
 results=$(psql -f ./scripts/test-the-release/find-tables.sql)
 
