d2a0e7c9d77c7bbb6760080b54e26e1012fe70cd
Lucas Krebs-Pinto <lucaspinto33@gmail.com>
2019-12-12 15:53:18 +0100

    change script

--- scripts/test-the-release.sh
+++ scripts/test-the-release.sh
@@ -5,17 +5,10 @@ IFS=$'\n\t'
 
 set -o nounset
 
-export PGHOST=${PGHOST}
 export PGPORT=5524
 export PGPASSWORD=${PGPASSWORD}
 export PGUSER="pgadmin"
 export PGDATABASE="sandboxes"
-
-if [[ -z $PGPASSWORD ]]; then
-  echo "No password found in CredHub! Is your deployment generating a value for 'pgadmin_database_password'?"
-  echo "Try to deployment with the templates/operations/set-properties.yml ops file."
-  exit 1
-fi
 
 results=$(psql -f ./scripts/test-the-release/find-tables.sql)
 
